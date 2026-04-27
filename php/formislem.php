859e89f2e472b79b29649c0a719636abe36f6106
Arda Dilek <ardavolllley25@gmail.com>
2026-04-27 22:41:02 +0300

    Genel tekrarlar duzeltildi ve guncellendi.

--- php/formislem.php
+++ php/formislem.php
@@ -47,33 +47,34 @@
                     <hr>
 
                     <?php
-                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                        // Verileri POST ile tek tek alma (Hoca için açıklayıcı yapı)
-                        $ad       = isset($_POST['ad']) ? htmlspecialchars($_POST['ad']) : 'Belirtilmedi';
-                        $email    = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : 'Belirtilmedi';
-                        $tel      = isset($_POST['tel']) ? htmlspecialchars($_POST['tel']) : 'Belirtilmedi';
-                        $konu     = isset($_POST['konu']) ? htmlspecialchars($_POST['konu']) : 'Belirtilmedi';
-                        $cinsiyet = isset($_POST['cinsiyet']) ? htmlspecialchars($_POST['cinsiyet']) : 'Belirtilmedi';
-                        $mesaj    = isset($_POST['mesaj']) ? htmlspecialchars($_POST['mesaj']) : 'Belirtilmedi';
-                        
-                        // Checkbox (Dizi) kontrolü
-                        $ilgiler = isset($_POST['ilgi']) ? $_POST['ilgi'] : [];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Verileri POST ile tek tek alma 
+    $ad = isset($_POST['ad']) ? htmlspecialchars($_POST['ad']) : 'Belirtilmedi';
+    $email = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : 'Belirtilmedi';
+    $tel = isset($_POST['tel']) ? htmlspecialchars($_POST['tel']) : 'Belirtilmedi';
+    $konu = isset($_POST['konu']) ? htmlspecialchars($_POST['konu']) : 'Belirtilmedi';
+    $cinsiyet = isset($_POST['cinsiyet']) ? htmlspecialchars($_POST['cinsiyet']) : 'Belirtilmedi';
+    $mesaj = isset($_POST['mesaj']) ? htmlspecialchars($_POST['mesaj']) : 'Belirtilmedi';
 
-                        echo "<div class='mb-3'><span class='label'>Ad Soyad:</span> <span class='value'>$ad</span></div>";
-                        echo "<div class='mb-3'><span class='label'>E-posta:</span> <span class='value'>$email</span></div>";
-                        echo "<div class='mb-3'><span class='label'>Telefon:</span> <span class='value'>$tel</span></div>";
-                        echo "<div class='mb-3'><span class='label'>Konu:</span> <span class='value'>$konu</span></div>";
-                        echo "<div class='mb-3'><span class='label'>Cinsiyet:</span> <span class='value'>$cinsiyet</span></div>";
-                        
-                        echo "<div class='mb-3'><span class='label'>İlgi Alanları:</span> <span class='value'>";
-                        echo !empty($ilgiler) ? implode(", ", array_map('htmlspecialchars', $ilgiler)) : 'Seçilmedi';
-                        echo "</span></div>";
-                        
-                        echo "<div class='mb-3'><span class='label'>Mesaj:</span><br><div class='p-3 border rounded bg-light mt-2 value'>$mesaj</div></div>";
-                    } else {
-                        echo "<div class='alert alert-danger'>Hata: Form verileri POST yöntemiyle gönderilmedi!</div>";
-                    }
-                    ?>
+    // Checkbox (Dizi) kontrolü
+    $ilgiler = isset($_POST['ilgi']) ? $_POST['ilgi'] : [];
+
+    echo "<div class='mb-3'><span class='label'>Ad Soyad:</span> <span class='value'>$ad</span></div>";
+    echo "<div class='mb-3'><span class='label'>E-posta:</span> <span class='value'>$email</span></div>";
+    echo "<div class='mb-3'><span class='label'>Telefon:</span> <span class='value'>$tel</span></div>";
+    echo "<div class='mb-3'><span class='label'>Konu:</span> <span class='value'>$konu</span></div>";
+    echo "<div class='mb-3'><span class='label'>Cinsiyet:</span> <span class='value'>$cinsiyet</span></div>";
+
+    echo "<div class='mb-3'><span class='label'>İlgi Alanları:</span> <span class='value'>";
+    echo !empty($ilgiler) ? implode(", ", array_map('htmlspecialchars', $ilgiler)) : 'Seçilmedi';
+    echo "</span></div>";
+
+    echo "<div class='mb-3'><span class='label'>Mesaj:</span><br><div class='p-3 border rounded bg-light mt-2 value'>$mesaj</div></div>";
+}
+else {
+    echo "<div class='alert alert-danger'>Hata: Form verileri POST yöntemiyle gönderilmedi!</div>";
+}
+?>
 
                     <div class="mt-4">
                         <a href="../iletisim.html" class="btn btn-secondary">Geri Dön</a>
